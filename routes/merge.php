3c59685d60670476bf231895a0758734821abdaf
Munim <munimwaleed@gmail.com>
2026-02-06 13:24:14 +0500

    by munim

---
+++ routes/merge.php
@@ -0,0 +1,21 @@
+//Route::controller(AdvisorController::class)->group(function () {
+//    Route::get('/dashboard', 'dashboard');
+//    Route::get('/application', 'application');
+//    Route::get('/courses', 'courses');
+//    Route::get('/grades', 'grades');
+//    Route::get('/progress', 'progress');
+//    Route::get('/issue', 'issue');
+//    Route::get('/manage_profile', 'profile');
+//});
+
+//Route::controller(StudentController::class)->group(function () {
+//    Route::get('/dashboardstudent', 'dashboardstudent');
+//    Route::get('/applicationstudent', 'applicationstudent');
+//    Route::get('/gradesstudent', 'gradesstudent');
+//    Route::get('/conflictsstudent', 'conflictsstudent');
+//    Route::get('/timetableclashstudent', 'timetableclashstudent');
+//    Route::get('/extraactivitystudent', 'extraactivitystudent');
+//    Route::get('/scholershipstudent', 'scholershipstudent');
+//    Route::get('/profilestudent', 'profile\student');
+//});
+//
